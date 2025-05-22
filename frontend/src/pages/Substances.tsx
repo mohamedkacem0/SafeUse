6410a5f07fa306d8c8fc2ff3cb48bada420cfd89
alex2 <alex2@gmail.com>
2025-05-23 00:14:14 +0200

    fix

--- frontend/src/pages/Substances.tsx
+++ frontend/src/pages/Substances.tsx
@@ -12,7 +12,6 @@ interface ApiSubstance {
   Formula: string;
 }
 
-/** Cómo la usará el componente Card */
 interface Drug {
   name: string;
   imageSrc: string;
@@ -20,7 +19,6 @@ interface Drug {
   formula: string;
 }
 
-/* ------------ 2. Componente -------------------- */
 
 export default function SubstancesPage() {
   const [query, setQuery]   = useState("");
@@ -30,10 +28,8 @@ export default function SubstancesPage() {
     fetch(
       "http://localhost/alex/SAFEUSE/SafeUse/backend/api/public/index.php?route=api/sustancias"
     )
-      // indicamos a TS qué va a venir: Promise<ApiSubstance[]>
       .then<ApiSubstance[]>(res => res.json())
       .then(apiData => {
-        /* 3. Mapeamos con tipado → Drug[] */
         const parsed: Drug[] = apiData.map((s) => ({
           name:     s.Nombre,
           imageSrc: s.Imagen,
@@ -62,7 +58,6 @@ export default function SubstancesPage() {
           </p>
         </div>
         <section className="flex flex-col items-center mt-10 p-6 gap-8 max-w-5xl mx-auto">
-          {/* Search input */}
           <div className="relative w-full max-w-md">
             <input
               type="text"
@@ -74,7 +69,6 @@ export default function SubstancesPage() {
             <Search className="absolute right-4 top-1/2 -translate-y-1/2 w-6 h-6 text-gray-700" />
           </div>
 
-          {/* Card grid */}
           <div className="grid grid-cols-1 sm:grid-cols-2 gap-[100px] ">
             {filtered.slice(0, 4).map((drug) => (
               <DrugCard key={drug.name} {...drug} />
