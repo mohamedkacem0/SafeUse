c60efc10d8209ec1bff1ad80e6b445e8bccafa3f
Mohamed Hadj kacem <mohamed.h.kacem@criterian.es>
2025-05-29 16:22:41 +0200

    contact submisson fix

--- backend/api/public/index.php
+++ backend/api/public/index.php
@@ -1,8 +1,5 @@
 <?php
 
-/*
-// DESCOMENTAR PARA FUNCIONAR
-
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
 header('Access-Control-Allow-Headers: Content-Type');
@@ -11,7 +8,7 @@ header('Access-Control-Allow-Headers: Content-Type');
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     exit(0);
 }
-    */
+
 // Carga manual sin Composer
 require_once __DIR__ . '/../app/core/Config.php';
 require_once __DIR__ . '/../app/core/DB.php';
