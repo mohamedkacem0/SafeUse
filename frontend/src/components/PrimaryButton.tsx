2a7613ee5166e1b11d2259a1c2d97e67abde7be4
Alejandro Martino <alejandro.d.martino@criterian.es>
2025-05-30 13:31:38 +0200

    mejora vista shop

--- frontend/src/components/PrimaryButton.tsx
+++ frontend/src/components/PrimaryButton.tsx
@@ -3,14 +3,16 @@ interface ButtonProps {
   className?: string;
   onClick?: () => void;
   type?: "button" | "submit" | "reset";
+  disabled?: boolean;
 }
 
-export default function PrimaryButton({ text, className, onClick, type = "button" }: ButtonProps) {
+export default function PrimaryButton({ text, className, onClick, type = "button", disabled = false }: ButtonProps) {
   return (
     <button
       type={type}
-      className={`w-[128px] h-[40px] bg-[#111111] text-white text-[16px] rounded-[20px] flex items-center justify-center hover:bg-neutral-800  ${className}`}
+      className={`w-[128px] h-[40px] bg-[#111111] text-white text-[16px] rounded-[20px] flex items-center justify-center hover:bg-neutral-800 ${className} ${disabled ? 'opacity-50 cursor-not-allowed' : ''}`}
       onClick={onClick}
+      disabled={disabled}
     >
       {text}
     </button>
