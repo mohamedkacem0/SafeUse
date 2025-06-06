d87fbd6bdba356a64557ede533a87e378ec4a7c7
Mohamed Hadj kacem <mohamed.h.kacem@criterian.es>
2025-06-06 14:15:19 +0200

    hosting changes 3

--- backend/api/app/core/config.php
+++ backend/api/app/core/config.php
@@ -11,10 +11,10 @@ class Config {
     */
 namespace App\Core;
 class Config {
-    public const DB_HOST = 'sql300.infinityfree.com';
-    public const DB_NAME = 'if0_39024092_safeuse3';
-    public const DB_USER = 'if0_39024092';
-    public const DB_PASS = 'safeuse123';
-    public const DSN     = 'mysql:host=sql300.infinityfree.com;port=3306;dbname=if0_39024092_safeuse3;charset=utf8mb4';
+    public const DB_HOST = 'bt8jynht5h0iztcpeayw-mysql.services.clever-cloud.com';
+    public const DB_NAME = 'bt8jynht5h0iztcpeayw';
+    public const DB_USER = 'u0kbjzesbwusdkke';
+    public const DB_PASS = '3ZsvM4cYO62nNxW22Dbh';
+    public const DSN     = 'mysql://u0kbjzesbwusdkke:3ZsvM4cYO62nNxW22Dbh@bt8jynht5h0iztcpeayw-mysql.services.clever-cloud.com:3306/bt8jynht5h0iztcpeayw';
 }
 
