9406b5ce6426c541c4c9e61ecd679ec686fc5d7f
Mohamed Hadj kacem <mohamed.h.kacem@criterian.es>
2025-06-09 15:45:23 +0200

    host changes 35

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,3 @@
-// vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
@@ -13,14 +12,25 @@ export default defineConfig({
       '/api': {
         target: 'https://safeuse-lkde.onrender.com',
         changeOrigin: true,
-        // convierte "/api/loquesea" en "?route=api/loquesea"
-        rewrite: (path) =>
-          path.replace(/^\/api\/(.+)/, '?route=api/$1'),
+        rewrite: (path) => path.replace(/^\/api\/(.+)/, '?route=api/$1'),
       },
     },
   },
   build: {
     outDir: path.resolve(__dirname, 'dist'),
     emptyOutDir: true,
+    chunkSizeWarningLimit: 1500, // Optional: avoid warnings for large chunks
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('react')) return 'vendor-react'
+            if (id.includes('lodash')) return 'vendor-lodash'
+            if (id.includes('axios')) return 'vendor-axios'
+            return 'vendor' // fallback for other node_modules
+          }
+        },
+      },
+    },
   },
 })
